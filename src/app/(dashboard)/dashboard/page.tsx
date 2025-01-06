12b42ecc2e7db04c970ee65a0c4bbada026fd45b
Sergio Andrés Oquendo Salazar <oliveratom11@hotmail.com>
2025-01-06 16:47:25 -0500

    feat: Update dashboard page to handle asynchronous search parameters

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import TransactionsTable from "@/components/transfers/TransactionsTable";
 
-export default function Page({
-  searchParams,
-}: {
-  searchParams: { q?: string };
-}) {
+export default async function Page(
+  props: {
+    searchParams: Promise<{ q?: string }>;
+  }
+) {
+  const searchParams = await props.searchParams;
   return <TransactionsTable searchParams={searchParams} />;
 }
